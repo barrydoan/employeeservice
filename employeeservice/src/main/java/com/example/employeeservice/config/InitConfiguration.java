30e9cdaaad6026075e177de75e53b969b45529c7
Nhat Doan <nhat.doan@sap.com>
2024-04-11 22:27:18 -0400

    add phone number

--- employeeservice/src/main/java/com/example/employeeservice/config/InitConfiguration.java
+++ employeeservice/src/main/java/com/example/employeeservice/config/InitConfiguration.java
@@ -30,26 +30,26 @@ public class InitConfiguration {
             System.out.println("Reloading " + departmentRepository.save(departments[4]));
             System.out.println("Reloading " + departmentRepository.save(departments[5]));
             // import employee
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(1), "James", "Smith", "js@gmail.com", "null", departments[0])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(2), "Robert", "Johnson", "rj@gmail.com", "", departments[0])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(3), "John", "Williams", "jw@gmail.com", "null", departments[0])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(4), "Michael", "Jones", "mj@gmail.com", "", departments[1])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(5), "David", "Garcia", "dg@gmail.com", "", departments[1])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(6), "William", "Miller", "wm@gmail.com", "", departments[1])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(7), "Richard", "Davis", "rd@gmail.com", "", departments[1])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(8), "Joseph", "Rodriguez", "jr@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(9), "Thomas", "Martinez", "tn@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(10), "Christopher", "Hernandez", "ch@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(11), "Charles", "Lopez", "cl@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(12), "Daniel", "Gonzales", "dg@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(13), "Matthew", "Wilson", "mw@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(14), "Anthony", "Anderson", "aa@gmail.com", "", departments[2])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(15), "Mark", "Thomas", "mt@gmail.com", "", departments[3])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(16), "Donald", "Taylor", "dt@gmail.com", "", departments[3])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(17), "Steven", "Moore", "sm@gmail.com", "", departments[3])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(18), "Andrew", "Jackson", "aj@gmail.com", "", departments[3])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(19), "Paul", "Lee", "pl@gmail.com", "", departments[3])));
-            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(20), "Joshua", "Perez", "jp@gmail.com", "", departments[3])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(1), "James", "Smith", "js@gmail.com", "(267) 421-0001", departments[0])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(2), "Robert", "Johnson", "rj@gmail.com", "(215) 421-0002", departments[0])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(3), "John", "Williams", "jw@gmail.com", "(090) 421-0003", departments[0])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(4), "Michael", "Jones", "mj@gmail.com", "(215) 421-0004", departments[1])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(5), "David", "Garcia", "dg@gmail.com", "(267) 421-0005", departments[1])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(6), "William", "Miller", "wm@gmail.com", "(267) 421-0006", departments[1])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(7), "Richard", "Davis", "rd@gmail.com", "(267) 421-0007", departments[1])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(8), "Joseph", "Rodriguez", "jr@gmail.com", "(267) 421-0008", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(9), "Thomas", "Martinez", "tn@gmail.com", "(267) 421-0009", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(10), "Christopher", "Hernandez", "ch@gmail.com", "(267) 421-0010", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(11), "Charles", "Lopez", "cl@gmail.com", "(267) 421-0011", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(12), "Daniel", "Gonzales", "dg@gmail.com", "(267) 421-0012", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(13), "Matthew", "Wilson", "mw@gmail.com", "(267) 421-0013", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(14), "Anthony", "Anderson", "aa@gmail.com", "(267) 421-0014", departments[2])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(15), "Mark", "Thomas", "mt@gmail.com", "(267) 421-0015", departments[3])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(16), "Donald", "Taylor", "dt@gmail.com", "(267) 421-0016", departments[3])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(17), "Steven", "Moore", "sm@gmail.com", "(267) 421-0017", departments[3])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(18), "Andrew", "Jackson", "aj@gmail.com", "(267) 421-0018", departments[3])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(19), "Paul", "Lee", "pl@gmail.com", "(267) 421-0019", departments[3])));
+            System.out.println("Reloading " + employeeRepository.save(new Employee(Long.valueOf(20), "Joshua", "Perez", "jp@gmail.com", "(267) 421-0012", departments[3])));
 
         };
     }
